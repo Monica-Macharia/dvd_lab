52d6e25d47c0db33787c8ada25ba8e2464b5cd97
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-10-01 05:27:09 +0300

    new api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+movies = Movie.create!([{ title: "Star Wars",
+    year: 2030, length: 1, description: "fiction", poster_url: "https://images.pexels.com/photos/6335954/pexels-photo-6335954.jpeg?auto=compress&cs=tinysrgb&w=300", category: "moon expedition", discount: true, female_director: true }, { title: "Utopia",
+        year: 2017, length: 2, description: "fiction", poster_url: "https://images.pexels.com/photos/8732608/pexels-photo-8732608.jpeg?auto=compress&cs=tinysrgb&w=300", category: "investigative", discount: true, female_director: false }, { title: "Rust",
+            year: 2016, length: 2.5, description: "Technology", poster_url: "https://images.pexels.com/photos/4310574/pexels-photo-4310574.jpeg?auto=compress&cs=tinysrgb&w=300", category: "Future", discount: false, female_director: false}])
